68f630f9b5ff3577a1a93a46cf3f2e36e4a77977
Aditya Porwal <aditya_porwal@epam.com>
2021-02-08 22:50:10 +0530

    Functions task 5 Complete
    Comment: split function

--- Functions/src/main/java/com/epam/engx/cleancode/functions/task5/DateUtil.java
+++ Functions/src/main/java/com/epam/engx/cleancode/functions/task5/DateUtil.java
@@ -8,12 +8,17 @@ public class DateUtil {
 	public Date changeToMidnight(Date date, boolean up) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
+		setTime(calender,up);
+		return calendar.getTime();
+	}
+	
+	private void setTime(Calender calender, boolean hasOffset) {
 		calendar.add(Calendar.DATE, up ? 1 : -1);
 		calendar.set(Calendar.HOUR_OF_DAY, 0);
 		calendar.set(Calendar.MINUTE, 0);
 		calendar.set(Calendar.SECOND, 0);
 		calendar.set(Calendar.MILLISECOND, 0);
-		return calendar.getTime();
 	}
+	
 
 }
